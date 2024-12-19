3e68ba4c4c910c81274f05ebaac8f07ab0eb4167
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2024-12-19 16:21:25 +0530

    Feat: Login/Register routes added

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -3,12 +3,21 @@ import {
   handleCreateUser,
   handleGetUser,
   handleEditUser,
+  handleGetAllUsers,
+  handleGetUserByEmail,
+  handleRegister,
+  handleLogin,
 } from "../controllers/UserController";
+import { authenticateToken } from "../middlewares/AuthMiddleware";
 
 const userRouter = express.Router();
 
-userRouter.post("/users", handleCreateUser);
-userRouter.get("/users/:id", handleGetUser);
-userRouter.put("/users/:id", handleEditUser);
+userRouter.post("/users/register", handleRegister);
+userRouter.post("/users/login", handleLogin);
+userRouter.post("/users", authenticateToken, handleCreateUser);
+userRouter.get("/users/id/:id", authenticateToken, handleGetUser);
+userRouter.get("/users/all", authenticateToken, handleGetAllUsers);
+userRouter.get("/users/email/:email", authenticateToken, handleGetUserByEmail);
+userRouter.put("/users/:id", authenticateToken, handleEditUser);
 
 export default userRouter;
