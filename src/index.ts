b7be8917a6620c657df9dd60b06352754f7ae7a2
UtkarshRastogi0712 <utkarshrastogi0712@gmail.com>
2024-12-27 12:04:30 +0530

    Feat: Peer Server setup

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,24 @@ import userRouter from "./routers/UserRouter";
 import { AppDataSource } from "./db/AppDataSource";
 import { createServer, Server as HTTPServer } from "http";
 import { Server as SocketServer } from "socket.io";
+import { ExpressPeerServer } from "peer";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const server: HTTPServer = createServer(app);
+const io: SocketServer = new SocketServer(server);
+const peerServer = ExpressPeerServer(server);
 
 app.use(express.json());
 app.use("/api", userRouter);
+app.use("/peerjs", peerServer);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server up on root!");
 });
-
-const server: HTTPServer = createServer(app);
-const io: SocketServer = new SocketServer(server);
 
 io.on("connection", () => {
   console.log("A User Connected");
